b04091d4afe293907e09f10e0f4c664a9a6ed681
Jagadesh200 <jagadeshmalapati@gmail.com>
2026-09-22 13:13:58 +0530

    updated api key

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 
-const apiKey = "4cce0e9417c123954e3fc4bfd0c6ec98";
+const apiKey = process.env.apiKey;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
